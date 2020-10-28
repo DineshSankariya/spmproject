e0040d4285d343a4d9ec1a546a6903ead36604f4
dinesh sankariya <dinesh.dina47@gmail.com>
2020-10-28 08:42:57 +0530

    my commit on 28th oct 2020

---
+++ src/main/resources/static/javascript/register.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+    $('#role').change(function(){
+
+        if($(this).val()=="Student"){
+               alert("Student");
+               $("#student_role").css("display","visible");
+        }
+
+
+    })
+
+
+
+
+
+})
